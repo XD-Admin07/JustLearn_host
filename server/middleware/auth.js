e06250206491fc5ab39f378e7bbf592220d3d1de
XD-Admin07 <chintuks.cks@gmail.com>
2023-10-17 00:50:35 +0530

    env auth

--- server/middleware/auth.js
+++ server/middleware/auth.js
@@ -1,7 +1,7 @@
 // Importing required modules
 const jwt = require("jsonwebtoken");
-const dotenv = require("dotenv");
-//const User = require("../models/User");
+//const dotenv = require("dotenv");
+const User = require("../models/User");
 // Configuring dotenv to load environment variables from .env file
 //dotenv.config();
 
