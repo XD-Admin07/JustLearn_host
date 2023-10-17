6bb56fe22c0ac14b1cf75336c5aa28dcde114702
XD-Admin07 <chintuks.cks@gmail.com>
2023-10-17 13:35:42 +0530

    link

--- server/mail/templates/courseEnrollmentEmail.js
+++ server/mail/templates/courseEnrollmentEmail.js
@@ -76,7 +76,7 @@ exports.courseEnrollmentEmail = (courseName, name) => {
                   are excited to have you as a participant!</p>
               <p>Please log in to your learning dashboard to access the course materials and start your learning journey.
               </p>
-              <a class="cta" href="">Go to Dashboard</a>
+              <a class="cta" href="https://just-learn-frontend.vercel.app/dashboard/my-profile">Go to Dashboard</a>
           </div>
           <div class="support">If you have any questions or need assistance, please feel free to reach out to us at <a
                href="mailto:info@justlearn.com">info@justlearn.com</a>. We are here to help!</div>
